4ee01e6342f29f867d279b514035c86d76599502
kaoru369 <simoon.kaoru@gmail.com>
2025-06-05 15:21:41 +0900

    kadai

---
+++ 01_1_Light_LED_kadai_copy/01_1_Light_LED_kadai_copy.ino
@@ -0,0 +1,37 @@
+const int litsnsrPin = 3; //3:ConnectorA 4:ConnectorB
+const int ledPin = 4; //3:ConnectorA 4:ConnectorB 10:Builtin
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(litsnsrPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() { 
+
+  float litsnsr_ad = analogRead(litsnsrPin); // Read analog data
+  float litsnsr_v = litsnsr_ad * 3.3 / 4096; // Calculation of voltage value
+  float lux = 10000 * litsnsr_v / (3.3 - litsnsr_v) / 1000; // Calculation of lux value
+
+  //Look at the serial monitor
+  Serial.print(lux);
+  Serial.print(" Lux : ");
+  
+  // Change the threshold to suit your environment
+  if (lux > 20) { 
+    Serial.println("Bright");
+    delay(500);
+  } else {
+    Serial.println("Dark");
+    delay(500);
+
+  }if (lux < 20) {
+    digitalWrite(ledPin, HIGH); //ON
+    Serial.println("Pushed");
+    delay(500);
+  } else {
+    digitalWrite(ledPin, LOW); //OFF
+    Serial.println("Not Pushed");
+    delay(500);
+  }
+}
